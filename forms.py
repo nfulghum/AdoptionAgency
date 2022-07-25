4909741fdf9521ab9d95ecac12be3678f7215927
Nick Fulghum <nickfulghum92@gmail.com>
2022-07-25 14:02:59 -0500

    Project completed

--- forms.py
+++ forms.py
@@ -1,3 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
 from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
+
+
+class AddPetForm(FlaskForm):
+    """Add pets form"""
+
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = SelectField("Species", choices=[
+                          ("cat", "Cat"), ("dog", "Dog"), ("fish", "Fish")])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    age = IntegerField("Age", validators=[
+                       Optional(), NumberRange(min=0, max=10)])
+    notes = TextAreaField("Comments", validators=[Optional(), Length(min=5)])
+
+
+class EditPetForm(FlaskForm):
+    """Form for editing an existing pet."""
+
+    photo_url = StringField(
+        "Photo URL",
+        validators=[Optional(), URL()],
+    )
+
+    notes = TextAreaField(
+        "Comments",
+        validators=[Optional(), Length(min=10)],
+    )
+
+    available = BooleanField("Available?")
